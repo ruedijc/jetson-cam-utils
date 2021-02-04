5b738e895d58beefdf2b108b983441f056fcd1cc
labuser <ruedijc>
2021-02-04 13:27:22 -0600

    updated gpio to use i2c access methods

---
+++ util/spacely_p13-1_on.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# this sets P13-1  on CTI Spacely carrier , and turns P13-1/ sysfs 486 / GPIO 14 to 'on' state
+
+
+# NOTE: carrier P13-1 = P16 on ti i2c expander, which is controlled by 8-15 registers
+
+# carrier pin : P13-1
+# tca9539 pin : P16
+# sysfs number: 486
+# i2c address : 0x77
+# dir register: 0x07
+# in register : 0x01
+# out register: 0x03
+
+
+# using i2c access to 0x77 the GPIO breakout chip:
+# i2cset -y -f <bus> <addr> <regiser> <data>
+# regsiters for 0x77:
+#   0x00 input register, 0-7
+#   0x01 input register,  8-15
+#   0x02 output register, 0-7
+#   0x03 output resister, 8-15
+#   0x06 io direction, 0-7
+#   0x07 io direction, 8-15
+
+i2cset -y -f 0x00 0x77 0x07 0x00  #set direction register P13-1 to P13-8 as outputs
+
+
+i2cset -y -f 0x00 0x77 0x03 0xff  #set output register all on for P13-1 to P13-8
+
+
+
+ 
+
