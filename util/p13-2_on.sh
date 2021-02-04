bac1499e58b9c4c70e1d9644600283620e0da063
labuser <ruedijc>
2021-02-04 15:09:25 -0600

    added per-pin on-off scripts for spacely P13 GPIO outputs

---
+++ util/p13-2_on.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Set Spacely P13-2 to 'on' state
+
+#set the direction register P13-1-8 as outputs
+i2cset -y -f 0x00 0x77 0x07 0x00
+
+#get the existing output register contents
+OUTREG=$(i2cget -y -f 0x00 0x77 0x03)
+printf 'existing output register is : 0x%X \n' ${OUTREG}
+
+#AND the value of the pin with the existing register values
+HIREG=$(( ${OUTREG} | 2#10000000 ))
+
+# output bew register (should be 64 if current reg is 0)
+printf 'new output register will be : 0x%X \n' ${HIREG}
+
+i2cset -y -f 0x00 0x77 0x03 ${HIREG}
+
+#get the existing output register contents
+NEWREG=$(i2cget -y -f 0x00 0x77 0x03)  
+printf 'new output register is  : 0x%X \n' ${NEWREG}
+
