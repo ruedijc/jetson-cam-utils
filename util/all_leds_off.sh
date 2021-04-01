a06a0dd42f21e87f1750c314c43aec451a0eddee
jruedin <jruedin@nanohmics.com>
2021-04-01 18:05:15 -0500

    changing gpio to sysfs from i2c, merged again

--- util/all_leds_off.sh
+++ util/all_leds_off.sh
@@ -2,16 +2,16 @@
 
 # turn off all leds
 
-/home/labuser/development/jetson-cam-utils/util/led1_off.sh
+echo 0 > /sys/class/gpio/gpio222/value
 echo 0 > /var/hsi/led1_state
 
-/home/labuser/development/jetson-cam-utils/util/led2_off.sh
+echo 0 > /sys/class/gpio/gpio223/value
 echo 0 > /var/hsi/led2_state
 
-/home/labuser/development/jetson-cam-utils/util/led3_off.sh
+echo 0 > /sys/class/gpio/gpio224/value
 echo 0 > /var/hsi/led3_state
 
-/home/labuser/development/jetson-cam-utils/util/led4_off.sh
+echo 0 > /sys/class/gpio/gpio225/value
 echo 0 > /var/hsi/led4_state
 
 
--- util/all_leds_on.sh
+++ util/all_leds_on.sh
@@ -2,17 +2,15 @@
 
 # turn on all leds
 
-/home/labuser/development/jetson-cam-utils/util/led1_on.sh
+echo 1 > /sys/class/gpio/gpio222/value
 echo 1 > /var/hsi/led1_state
 
-/home/labuser/development/jetson-cam-utils/util/led2_on.sh
+echo 1 > /sys/class/gpio/gpio223/value
 echo 1 > /var/hsi/led2_state
 
-/home/labuser/development/jetson-cam-utils/util/led3_on.sh
+echo 1 > /sys/class/gpio/gpio224/value
 echo 1 > /var/hsi/led3_state
 
-/home/labuser/development/jetson-cam-utils/util/led4_on.sh
+echo 1 > /sys/class/gpio/gpio225/value
 echo 1 > /var/hsi/led4_state
-
-
 
