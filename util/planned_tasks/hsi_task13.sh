48db556b74a3ed54ce5fb32f1dc91b95e2f60d50
labuser <ruedijc@gmail.com>
2021-04-19 16:39:08 -0500

    updates to bash tasks

--- util/planned_tasks/hsi_task13.sh
+++ util/planned_tasks/hsi_task13.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # task13 - stop all tasks
-echo "stop all tasks"
-systemctl stop hsi*.service
+echo "stop all capturing tasks"
+sudo systemctl stop hsi_task3.timer
+sudo systemctl stop hsi_task4.timer
+sudo systemctl stop hsi_task9.timer
+sudo systemctl stop hsi_task10.timer
 
 # close shutter
 /home/labuser/development/jetson-cam-utils/util/shutter_close.sh
---
+++ util/planned_tasks/hsi_task6.sh
@@ -0,0 +1,44 @@
+#/bin/bash
+#
+# capture a single mipi-csi2 image to jpg
+#
+#
+# notes -  quality levels- h264 
+# at full 3840x2160 bitrate >=80000000
+# at 1920x1080 ,    bitrate >=8000000
+#
+
+DURATION=10 #recording duration in seconds
+TIME=$(date '+%Y-%m-%d.%H-%M-%S')
+LOCATION='/media/msata/queue/'
+FILE=$LOCATION$TIME'_hd_vid.h265'
+echo $FILE
+
+gst-launch-1.0 nvarguscamerasrc sensor-id=0  timeout=$DURATION !\
+'video/x-raw(memory:NVMM), width=1920, height=1080, format=NV12, framerate=30/1' !\
+nvvidconv flip-method=2 !\
+'video/x-raw(memory:NVMM), format=I420' !\
+omxh265enc peak-bitrate=8000000 !\
+'video/x-h265, stream-format=byte-stream' !\
+h265parse !\
+qtmux !\
+filesink location=$FILE -e
+
+# show size
+echo "==============================================="
+du -h $FILE
+# show vid folder size
+du -h $LOCATION
+echo "==============================================="
+
+
+#for development, now open/playback the video
+# take out for sapce use -
+
+#gst-launch-1.0 filesrc location=$FILE !\
+#qtdemux name=demux demux.video_0 !\
+#queue !\
+#h265parse ! omxh265dec !\
+#nveglglessink -e
+
+
